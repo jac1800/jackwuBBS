1f72a613f6006dbefdd1155c1c3374a60512d639
jackwu <403226004@qq.com>
2020-03-08 23:03:07 +0800

    邮箱验证

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,6 +35,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\EnsureUserEmailIsVerified::class,  //用户登录邮箱验证
         ],
 
         'api' => [
