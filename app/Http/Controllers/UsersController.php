ade8fc9a83f819d793be7df3fb074f9c114478cd
jackwu <403226004@qq.com>
2020-03-11 22:33:46 +0800

    upload image

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImagesUploadHandles;
 
 class UsersController extends Controller
 {
@@ -14,7 +15,7 @@ class UsersController extends Controller
          return view("users.show",compact('user'));
     }
 
-    public function update(UserRequest $request, User $user)
+    public function update(UserRequest $request, User $user,ImagesUploadHandles $img_upload)
     {
           //使用request的Validate来验证
 //        $validata=$request->validate([
@@ -23,12 +24,15 @@ class UsersController extends Controller
 //            "instruction"=>"required|max:255",
 //        ]);
         //自定义请求验证UserRequest
-        $rs=$user->update($request->all());
-        if($rs){
+        $data=$request->all();
 
-        }else{
-
+        if($request->avatar) {
+            $upload_rs = $img_upload->save($data["avatar"], 'avatar', $user->id);
+            if ($upload_rs) {
+                $data["avatar"] = $upload_rs['path'];
+            }
         }
+        $rs=$user->update($data);
         return redirect()->route("users.show",$user->id)->with('success',"编辑个人资料成功");
 
     }
