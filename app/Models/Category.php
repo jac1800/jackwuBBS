a792686327dfb5a7dd92cbcb53bc8c82646d372b
jackwu <403226004@qq.com>
2020-03-13 17:09:55 +0800

    话题列表

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,4 +10,9 @@ class Category extends Model
     public $timestamps=false;
 
     public $fillable=["name","description"];
+
+    public function Topic()
+    {
+        return $this->hasMany(Topic::class);
+    }
 }
