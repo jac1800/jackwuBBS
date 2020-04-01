5bdbeb991714e4371fea6cc19d4317362246a3a9
jackwu <403226004@qq.com>
2020-04-01 12:13:32 +0800

    流量限制

--- routes/api.php
+++ routes/api.php
@@ -17,10 +17,21 @@ use Illuminate\Http\Request;
 //    return $request->user();
 //});
 
-Route::prefix("v1")->namespace("Api")->name("api.v1.")->group(function (){
-    //短信验证
-    Route::post("verificationCodes","VerificationCodesController@store")->name("verificationCodes.store");
-    //用户注册
-    Route::post("users","UsersController@store")->name("users.store");
+Route::prefix("v1")
+    ->namespace("Api")
+    ->name("api.v1.")
+    ->group(function (){
+        Route::middleware("throttle:".config('api.rate_limits.sign'))
+            ->group(function () {
+                //短信验证
+                Route::post("verificationCodes","VerificationCodesController@store")->name("verificationCodes.store");
+                //用户注册
+                Route::post("users","UsersController@store")->name("users.store");
+            });
+        Route::middleware("throttle:".config("api.rate_limits.access"))
+            ->group(function () {
+
+            });
+
 });
 
