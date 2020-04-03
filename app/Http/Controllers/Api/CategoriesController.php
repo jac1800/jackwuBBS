4ee8bd6453f3a09ba15fe6a9341aeff7b4dceb6d
jackwu <403226004@qq.com>
2020-04-03 18:04:01 +0800

    修改帖子

--- app/Http/Controllers/Api/CategoriesController.php
+++ app/Http/Controllers/Api/CategoriesController.php
@@ -6,13 +6,13 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Http\Resources\CategoryResource;
+use Illuminate\Support\Facades\Auth;
 
 class CategoriesController extends Controller
 {
     //
     public function index()
     {
-
         CategoryResource::wrap('data');
         return CategoryResource::collection(Category::all());
     }
--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Http\Requests\TopicRequest;
 use App\Models\Topic;
 use App\Http\Resources\TopicResource;
+use App\Http\Requests\Api\TopicRequest;
 
 class TopicsController extends Controller
 {
@@ -17,6 +17,12 @@ class TopicsController extends Controller
         $topic->user_id = $request->user()->id;
         $topic->save();
 
+        return new TopicResource($topic);
+    }
+    public function update(TopicRequest $request, Topic $topic)
+    {
+        $this->authorize('update', $topic);//验证修改权限
+        $topic->update($request->all());
         return new TopicResource($topic);
     }
 }
